121e3fea11146fe4692ffde055e36ff0cad658f7
Katie Shermer <katieshermer@gmail.com>
2017-05-17 11:40:59 -0600

    Took out @ on instance variables where they were unnecessary

--- lib/credit_check_recheck.rb
+++ lib/credit_check_recheck.rb
@@ -16,18 +16,18 @@ class CreditCheckRecheck
   end
 
   def string_to_array
-    @card_number = @card_number.split("")
-    @card_number.to_a
-    @card_number.map! { |num| num.to_i}
+    @card_number = card_number.split("")
+    card_number.to_a
+    card_number.map! { |num| num.to_i}
   end
 
   def reverse
-    @card_number.reverse! unless @card_number.length.odd?
+    card_number.reverse! unless card_number.length.odd?
   end
 
   def double_every_other_odd
     if card_number.length.odd?
-      @card_number = @card_number.map.with_index do |num, index|
+      @card_number = card_number.map.with_index do |num, index|
         if index.odd?
           num*2
         else
@@ -39,7 +39,7 @@ class CreditCheckRecheck
 
   def double_every_other_even
     if card_number.length.even?
-      @card_number = @card_number.map.with_index do |num, index|
+      @card_number = card_number.map.with_index do |num, index|
         if index.even?
           num*2
         else
@@ -51,21 +51,17 @@ class CreditCheckRecheck
 
 
   def sum_double_digits
-    @card_number = @card_number.map do |num|
-      if num.digits.length > 1
-        num.digits.sum
-      else
-        num
-      end
+    @card_number = card_number.map do |num|
+      num.digits.sum
     end
   end
 
   def sum_all_digits
-    @card_number = @card_number.sum
+    @card_number = card_number.sum
   end
 
   def valid?
-    if @card_number % 10 == 0
+    if card_number % 10 == 0
       @valid = true
     end
     @valid
